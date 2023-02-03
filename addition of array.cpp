d2b176000293703455355f06064424acf1a91b79
OnkarNanadanwade2002 <124372381+OnkarNanadanwade2002@users.noreply.github.com>
2023-02-03 19:48:08 +0530

    Add files via upload

---
+++ addition of array.cpp
@@ -0,0 +1,49 @@
+/*
+#include<stdio.h>
+int main()
+{
+	int a[10],b[10],c[10],i,n;
+    printf("\n enter the size of array :");
+    scanf("%d",&n);
+    
+    printf("\n enter elements in A:");
+    {
+    	for(i=0;i<n;i++)
+    	scanf("%d",&a[i]);
+	}
+	
+	printf("\n enter elements in B :");
+	{
+		for(i=0;i<n;i++)
+		scanf("%d",&b[i]);
+	
+	}
+	
+	{
+		for(i=0;i<n;i++)
+		c[i]=a[i]+b[i];
+	}
+	
+	printf("\n addiotion :");
+	{
+		for(i=0;i<n;i++)
+		printf("\n %d",c[i]);
+	}
+
+
+}
+*/
+/*
+#include<stdio.h>
+int main()
+{
+	int a,b,sum,sub;
+		printf("\n enter the two no :");
+	scanf("%d%d",&a,&b);
+  sum=a+b;
+  sub=a-b;
+
+	printf("\n sum=%d",sum);
+	printf("\n sub=%d",sub);
+}
+*/
